f8c03173cbd7600d7dba5f0ff8fa6ef6ff272ea1
bronku <bronk.jakub@proton.me>
2025-03-28 18:12:00 +0100

    moved error page to logging

--- internal/logging/error.go
+++ internal/logging/error.go
@@ -1,11 +1,11 @@
-package server
+package logging
 
 import (
 	"fmt"
 	"net/http"
 )
 
-func errorPage(err error, status int) http.HandlerFunc {
+func ErrorPage(err error, status int) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(status)
 		fmt.Fprint(w, err.Error())
